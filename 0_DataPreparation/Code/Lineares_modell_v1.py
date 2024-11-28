813425fa41087152230a728dbf9e847951ef607b
Moritz <166653101+MoritzHi@users.noreply.github.com>
2024-11-28 16:20:46 +0000

    2 Modelle, 1mal Temperatur, 1mal Wettercodes

--- 0_DataPreparation/Code/Lineares_modell_v1.py
+++ 0_DataPreparation/Code/Lineares_modell_v1.py
@@ -3,10 +3,15 @@
 import pandas as pd
 import statsmodels.formula.api as smf
 
-umsatz = pd.read_csv('/workspaces/Bakery-Project/0_DataPreparation/Data/final_umsatz.csv')
+umsatz = pd.read_csv('/workspaces/Bakery-Project/0_DataPreparation/Data/umsatz_gesamt.csv')
 umsatz.head()
 
-# Fit the linear model
+# Fit the linear model zu Temperatur
 mod = smf.ols('Umsatz ~ Temperatur', data=umsatz).fit()
+# Print the summary
+print(mod.summary())
+
+# Fit the linear model zu Wettercodes
+mod = smf.ols('Umsatz ~ wetter_sonnig + wetter_wolken + wetter_regen + wetter_schnee + wetter_gewitter + wetter_dunst + wetter_nebel', data=umsatz).fit()
 # Print the summary
 print(mod.summary())
