937253c2c90fa31e2d473c45766305803c1ba090
Moritz <166653101+MoritzHi@users.noreply.github.com>
2024-12-04 17:40:17 +0000

    Lineares Modell mit optimierten adj. r squared

--- 0_DataPreparation/Code/Lineares_modell_v1.py
+++ 0_DataPreparation/Code/Lineares_modell_v1.py
@@ -5,23 +5,20 @@ import statsmodels.formula.api as smf
 
 umsatz = pd.read_csv('/workspaces/Bakery-Project/0_DataPreparation/Data/umsatz_gesamt.csv')
 umsatz.head()
+print(umsatz)
 
-# Fit the linear model zu Temperatur
-mod = smf.ols('Umsatz ~ Warengruppe', data=umsatz).fit()
+# we want to maximize Adj. R-squared ideal close to 1 because it tells us how much of the variance is explained
+# coef tells us how much the value moves if the dependet value increases by 1
+# last 2 columns are the 95% confidence interval
+# 'Umsatz' is the dependent variable (target) we aim to predict
+# 'Wochentag' is the continuous feature
+# 'C(condition)' treats the 'Warengruppe' feature as a categorical variable.
+
+# Fit the linear model
+mod = smf.ols('Umsatz ~ Wochentag + C(Warengruppe) + C(KielerWoche)', data=umsatz).fit()
 # Print the summary
 print(mod.summary())
 
-# Fit the linear model zu Temperatur
-mod = smf.ols('Umsatz ~ Temperatur', data=umsatz).fit()
-# Print the summary
-print(mod.summary())
-
-# Fit the linear model zu Wettercodes
-mod = smf.ols('Umsatz ~ wetter_sonnig + wetter_wolken + wetter_regen + wetter_schnee + wetter_gewitter + wetter_dunst + wetter_nebel', data=umsatz).fit()
-# Print the summary
-print(mod.summary())
-
-# Fit the linear model zu Wochentag
-mod = smf.ols('Umsatz ~ Wochentag', data=umsatz).fit()
+mod = smf.ols('Umsatz ~ Wochentag + C(Warengruppe) + C(KielerWoche) + C(Monat) + C(wetter_sonnig) + C(wetter_wolken) + C(wetter_regen) + C(wetter_schnee) + C(wetter_gewitter) + C(wetter_dunst) + C(wetter_nebel)', data=umsatz).fit()
 # Print the summary
 print(mod.summary())
