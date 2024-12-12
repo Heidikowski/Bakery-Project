fdda8fba5c63898b7d02aab16761492f4b1458e9
Moritz <166653101+MoritzHi@users.noreply.github.com>
2024-12-12 14:54:01 +0000

    Fitted model with all parameters that we have and make sense so far

--- 0_DataPreparation/Code/Lineares_modell_v1.py
+++ 0_DataPreparation/Code/Lineares_modell_v1.py
@@ -3,9 +3,24 @@
 import pandas as pd
 import statsmodels.formula.api as smf
 
-umsatz = pd.read_csv('/workspaces/Bakery-Project/0_DataPreparation/Data/umsatz_gesamt.csv')
+umsatz = pd.read_csv('/workspaces/Bakery-Project/0_DataPreparation/Data/umsatz_gesamt_feiertage.csv')
 umsatz.head()
 print(umsatz)
+umsatz.columns
+
+bins = [-10, 10, 20, 35]  
+labels = ['Kalt', 'Moderat', 'Warm', ]
+
+# Temperatur-Bins der Spalte hinzufügen
+umsatz['Temperatur_Bin'] = pd.cut(umsatz['Temperatur'], bins=bins, labels=labels, include_lowest=True)
+
+# Kontrolle der Verteilung in den Bins
+print(umsatz['Temperatur_Bin'].value_counts())
+
+print(umsatz.head())
+
+umsatz.columns
+
 
 # we want to maximize Adj. R-squared ideal close to 1 because it tells us how much of the variance is explained
 # coef tells us how much the value moves if the dependet value increases by 1
@@ -19,6 +34,12 @@ mod = smf.ols('Umsatz ~ Wochentag + C(Warengruppe) + C(KielerWoche)', data=umsat
 # Print the summary
 print(mod.summary())
 
-mod = smf.ols('Umsatz ~ Wochentag + C(Warengruppe) + C(KielerWoche) + C(Monat) + C(wetter_sonnig) + C(wetter_wolken) + C(wetter_regen) + C(wetter_schnee) + C(wetter_gewitter) + C(wetter_dunst) + C(wetter_nebel)', data=umsatz).fit()
+# Everything except Kategorie, Bewoelkung, Temperatur, Windgeschwindigkeit, Wettercode and Temperatur_Bin
+mod = smf.ols('Umsatz ~ Wochentag + C(Warengruppe) + C(KielerWoche) + C(Monat) + C(wetter_sonnig) + C(wetter_wolken) + C(wetter_regen) + C(wetter_schnee) + C(wetter_gewitter) + C(wetter_dunst) + C(wetter_nebel) + C(sunday_or_holiday)', data=umsatz).fit()
+# Print the summary
+print(mod.summary())
+
+# Everything except Kategorie, Bewoelkung, Temperatur, Windgeschwindigkeit, Wettercode
+mod = smf.ols('Umsatz ~ Wochentag + C(Warengruppe) + C(KielerWoche) + C(Monat) + C(wetter_sonnig) + C(wetter_wolken) + C(wetter_regen) + C(wetter_schnee) + C(wetter_gewitter) + C(wetter_dunst) + C(wetter_nebel) + C(sunday_or_holiday) + C(Temperatur_Bin)', data=umsatz).fit()
 # Print the summary
 print(mod.summary())
